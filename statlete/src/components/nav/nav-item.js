fd69193558b74e0886abee7c2ccdaf066471d0ad
Dalton Wignall <dalton@semcat.net>
2019-07-20 19:15:08 -0500

    -Fix small styling issues with react router link and player menu items

--- statlete/src/components/nav/nav-item.js
+++ statlete/src/components/nav/nav-item.js
@@ -4,7 +4,7 @@ import Link from "react-router-dom/es/Link";
 import "./styles.scss";
 
 const NavItem = ({ title, imagePath }) => (
-  <Link to="/view-athlete">
+  <Link to="/view-athlete" className="nav__link">
     <li className="nav__item">
       <img className="nav__item__image" src="https://i.dailymail.co.uk/i/pix/2015/04/26/00/2800DADF00000578-3055692-image-a-20_1430006228344.jpg" alt={title} />
       <span className="nav__item__title">{title}</span>
